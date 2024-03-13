c7dc3651d3483175c514080e26778a8181552252
nikita-kotliar <knr200710@gmail.com>
2024-03-13 13:32:18 +0200

    update

--- src/js/darkThemeBtn.js
+++ src/js/darkThemeBtn.js
@@ -1,17 +1,18 @@
 let checkbox = null;
 const themeStylesheet = document.getElementById('theme-stylesheet');
-document.addEventListener('DOMContentLoaded', function () {
-  checkbox =
-    window.innerWidth > 1440
-      ? document.getElementById('checkbox')
-      : document.getElementById('theme-checkbox');
 
+document.addEventListener('DOMContentLoaded', function () {
   const savedTheme = localStorage.getItem('theme');
   if (savedTheme === 'dark') {
     setDarkTheme();
   } else {
     setLightTheme();
   }
+
+  checkbox =
+    window.innerWidth > 1440
+      ? document.getElementById('checkbox')
+      : document.getElementById('theme-checkbox');
 
   checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
